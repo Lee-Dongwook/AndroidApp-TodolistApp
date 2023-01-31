269d70b367fad5b7ce97fd5e055cfc154f098682
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-02-01 02:18:35 +0900

    git commit
    첫번째 안드로이드 앱 개발

---
+++ app/src/main/java/com/example/todolistapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.todolistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et_id;
+    EditText et_pw;
+    Button btn_test;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et_id = findViewById(R.id.et_id);
+        et_pw = findViewById(R.id.et_pw);
+        btn_test = findViewById(R.id.btn_test);
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Calendar.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
