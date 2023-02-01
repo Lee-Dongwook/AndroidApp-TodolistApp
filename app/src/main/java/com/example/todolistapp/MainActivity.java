9b56d86df5d9839cee8c50e1a207da3b11600e52
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-02-01 19:35:47 +0900

    git commit
    두번째 안드로이드 앱 개발

--- app/src/main/java/com/example/todolistapp/MainActivity.java
+++ app/src/main/java/com/example/todolistapp/MainActivity.java
@@ -1,45 +1,61 @@
 package com.example.todolistapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText et_id;
-    EditText et_pw;
-    Button btn_test;
-
-    Button btn_reg;
+    private RecyclerView mRv_todo;
+    private FloatingActionButton mBtn_write;
+    private ArrayList<Todoitem> mTodoItems;
+    private DBHelper mDBHelper;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et_id = findViewById(R.id.et_id);
-        et_pw = findViewById(R.id.et_pw);
-        btn_test = findViewById(R.id.btn_test);
-        btn_reg = findViewById(R.id.btn_reg);
-        btn_test.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Calendar.class);
-                startActivity(intent);
-            }
-        });
-
-        btn_reg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Register.class);
-                startActivity(intent);
-            }
-        });
-
+        setInit();
     }
+
+    private void setInit()
+    {
+        mDBHelper = new DBHelper(this);
+        mRv_todo = findViewById(R.id.rv_todo);
+        mBtn_write = findViewById(R.id.btn_write);
+        mTodoItems = new ArrayList<>();
+
+        mBtn_write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = new Dialog(MainActivity.this,android.R.style.Theme_Material_Dialog);
+                dialog.setContentView(R.layout.dialog_edit);
+                EditText et_title = dialog.findViewById(R.id.et_title);
+                EditText et_content = dialog.findViewById(R.id.et_content);
+                Button btn_ok = dialog.findViewById(R.id.btn_ok);
+                btn_ok.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String currentTime = new SimpleDateFormat("yyyy-MM-dd HH::mm::ss").format(new Date());
+                        mDBHelper.Insert(et_title.getText().toString(),et_content.getText().toString(),currentTime);
+
+                    }
+                });
+            }
+        });
+    }
+
+
 }
